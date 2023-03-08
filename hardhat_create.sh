f89d95d6ff2ee83946a260d2ca19ad12dda93727
Tom.Riddle <Tom.Riddle@gmail.com>
2023-03-08 14:26:10 +0800

    更新脚本

--- hardhat_create.sh
+++ hardhat_create.sh
@@ -9,9 +9,9 @@ cd $1
 
 ## git 操作
 git init
-wget https://raw.githubusercontent.com/blockchain-pro/xd-share/main/.gitignore
-wget https://raw.githubusercontent.com/blockchain-pro/xd-share/main/README.md
-wget https://raw.githubusercontent.com/blockchain-pro/xd-share/main/process_config
+wget -N https://raw.githubusercontent.com/blockchain-pro/xd-share/main/.gitignore
+wget -N https://raw.githubusercontent.com/blockchain-pro/xd-share/main/README.md
+wget -N https://raw.githubusercontent.com/blockchain-pro/xd-share/main/process_config
 chmod 777 process_config
 git add .
 git commit -m "init"
